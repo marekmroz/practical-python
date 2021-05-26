4490080f01f2f854c43e79c5514138ec4776d763
Marek Mroz <m2mroz@hotmail.com>
2021-05-26 23:22:00 +0100

    solve bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,17 @@
 # bounce.py
 #
 # Exercise 1.5
+
+"""
+A rubber ball is dropped from a height of 100 meters and each time it hits the
+ground, it bounces back up to 3/5 the height it fell. Write a program bounce.py
+ that prints a table showing the height of the first 10 bounces.
+"""
+
+height = 100.0
+bounce_factor = 0.6
+bounce_count = 10
+
+for i in range(bounce_count):
+    height *= bounce_factor
+    print(i + 1, round(height, ndigits=4))
