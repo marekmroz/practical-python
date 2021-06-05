d6a79a4aaa23ba100b731420d5bc6cd7808a3860
Marek Mroz <m2mroz@hotmail.com>
2021-06-05 17:31:28 +0100

    exercise 1.9 with adjustment for 0-based month indexing

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,12 +7,18 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
+
 month = 0
 
 while principal > 0:
-    _payment = payment + 1000.0 if month < 12 else payment
+    extra_payment_range = range(extra_payment_start_month, extra_payment_end_month)
+    _payment = payment + extra_payment if month in extra_payment_range else payment
     principal = principal * (1+rate/12) - _payment
     total_paid = total_paid + _payment
     month += 1
 
-print('Total paid', round(total_paid, ndigits=2), 'Months', month)
+print('Total paid', round(total_paid, ndigits=2))
+print('Months', month)
